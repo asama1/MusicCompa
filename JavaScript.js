fa1f7f2a25f588d8fad4464d9ea9e2334d295bc1
asama1 <79220179+asama1@users.noreply.github.com>
2022-04-20 22:01:22 -0400

    Create JavaScript.js

---
+++ JavaScript.js
@@ -0,0 +1,65 @@
+// Define variables
+let degrees = 0
+
+basic.forever(function () {
+    // Set 'degrees' variable as the direction of device in degrees
+    degrees = input.compassHeading()
+
+    // Check if device is facing north (45 < x > 315)
+    if (degrees < 45 || degrees > 315) {
+        // Display the letter 'N'
+        basic.showString("N")
+
+        // Stop playing current track
+        music.stopAllSounds()
+
+        // Play Jingle Bells
+        music.playMelody("E - E - E E E - ", 280)
+        music.playMelody("E - E - E E E - ", 280)
+        music.playMelody("E - G - C - D - ", 280)
+        music.playMelody("E E E E - - - - ", 280)
+        music.playMelody("F - F - F F F - ", 280)
+        music.playMelody("F - E - E E E - ", 280)
+        music.playMelody("E - E - G - G - ", 280)
+        music.playMelody("F - D - C C C - ", 280)
+        music.playMelody("C5 C5 C5 - - - - - ", 280)
+    }
+
+    // Check if device is facing west (135 < x)
+    else if (degrees < 135) {
+        // Display the letter 'W'
+        basic.showString("W")
+
+        // Stop playing current track
+        music.stopAllSounds()
+
+        // Play Super Mario Theme
+        music.playMelody("E - E - E E E - ", 280)
+    }
+
+    // Check if device is facing south (225 < x)
+    else if (degrees < 225) {
+        // Display the letter 'S'
+        basic.showString("S")
+
+        // Stop playing current track
+        music.stopAllSounds()
+
+        // Play Never Gonna Give You Up
+        music.playMelody("E - E - E E E - ", 280)
+    }
+
+    // Otherwise, device is assumed to be facing east
+    else {
+        // Display the letter 'E'
+        basic.showString("E")
+
+        // Stop playing current track
+        music.stopAllSounds()
+
+        // Play Bloody Stream
+        music.playMelody("E - E - E E E - ", 280)
+    }
+})
+
+
